3cf8e167e81efd0192a4d693ad2718cb04129616
gajindubandara <gajindukb@gmail.com>
2024-11-20 11:27:00 +0530

    Test

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,29 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 
-function App() {
+// Importing pages
+import Home from './pages/Home';
+import About from './pages/About';
+import Contact from './pages/Contact';
+
+const App: React.FC = () => {
   return (
-    <h1>Deployment works</h1>
+      <Router>
+        <nav>
+          <ul>
+            <li><Link to="/">Home</Link></li>
+            <li><Link to="/about">About</Link></li>
+            <li><Link to="/contact">Contact</Link></li>
+          </ul>
+        </nav>
 
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+      </Router>
   );
-}
+};
 
 export default App;
